d3e6363668d27264be619866c20ea01160d2fa5a
Nesk <pardanaud.j+mailforrobots@gmail.com>
2012-08-19 15:06:26 +0200

    Added Connect favicon and removed an useless CSS property

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var connect = require('connect'),
     wheat = require('wheat');
 
 var app = connect()
+	.use(connect.favicon())
     .use(connect.logger(':date | :remote-addr | :referrer | HTTP/:http-version | :method | :url | :status | :res[content-length] | :response-time | :user-agent'))
     .use(wheat())
 .listen(3001, '127.0.0.1');
