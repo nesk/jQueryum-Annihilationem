95e80b7eb08594c00740fbec83365cca165254a7
Nesk <pardanaud.j+mailforrobots@gmail.com>
2012-08-26 16:30:02 +0200

    Now displays a message in the console when the server is listening

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ var app = connect()
 	.use(connect.favicon())
     .use(connect.logger(':date | :remote-addr | :referrer | HTTP/:http-version | :method | :url | :status | :res[content-length] | :response-time | :user-agent'))
     .use(wheat(__dirname))
-.listen(3001, '127.0.0.1');
+.listen(3001, '127.0.0.1', function() {
+	console.log('Listening on 127.0.0.1:3001');
+});
